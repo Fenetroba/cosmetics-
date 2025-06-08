6778fe1c52e1ff14604edff618f5008d0243ea72
fenet roba <fenetroba700@gmail.com>
2025-06-08 16:58:27 +0300

    front

--- frontend/src/Components/ui/sidebar.jsx
+++ frontend/src/Components/ui/sidebar.jsx
@@ -5,17 +5,17 @@ import { PanelLeftIcon } from "lucide-react"
 
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Separator } from "@/components/ui/separator"
+import { Button } from "@/Components/ui/button"
+import { Input } from "@/Components/ui/input"
+import { Separator } from "@/Components/ui/separator"
 import {
   Sheet,
   SheetContent,
   SheetDescription,
   SheetHeader,
   SheetTitle,
-} from "@/components/ui/sheet"
-import { Skeleton } from "@/components/ui/skeleton"
+} from "@/Components/ui/sheet"
+import { Skeleton } from "@/Components/ui/skeleton"
 import {
   Tooltip,
   TooltipContent,
