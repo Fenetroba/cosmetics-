6720c48cd8e307c422cb54433dd84ce0ea74d035
fenet roba <fenetroba700@gmail.com>
2025-06-08 21:57:33 +0300

    front

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,24 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase warning limit to 1000kb
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split vendor chunks
+          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+          'vendor-ui': ['@headlessui/react', '@heroicons/react'],
+          'vendor-utils': ['axios', 'date-fns', 'formik', 'yup'],
+          'vendor-redux': ['@reduxjs/toolkit', 'react-redux'],
+        },
+      },
     },
   },
 })
